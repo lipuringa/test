dc9577932cf72b7dcbd69bcc34a6e09e342aee72
unknown <s.lipuringa@ya.ru>
2024-11-01 14:03:48 +0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    @vite(['resources/css/styles.scss'])
+</head>
+<body>
+    <div>   
+        <h1>Hallo!!! <strong>123</strong></h1>
+    </div>
+</body>
+</html>
